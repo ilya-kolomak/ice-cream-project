09ac6acc001de61091a3ddebcacd9358854cb63b
ilya-kolomak <99127402+ilya-kolomak@users.noreply.github.com>
2022-07-17 17:02:41 +0000

    Deploying to gh-pages from @ ilya-kolomak/ice-cream-project@ca61da21d9c9cbe318f85d2b92cc77f432f88db4 🚀

---
+++ index.0e307ad2.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelectorAll("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.forEach((function(e){return e.addEventListener("click",o)})),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.0e307ad2.js.map
