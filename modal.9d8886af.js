43e692ffa1cc8c3e2b151a5f96c7608c452b94d6
aleksnedo <104201289+aleksnedo@users.noreply.github.com>
2022-07-18 13:31:38 +0000

    Deploying to gh-pages from @ ilya-kolomak/ice-cream-project@cc6297fb1267509ca5b0d1049fb214fb9da2c269 🚀

---
+++ modal.9d8886af.js
@@ -0,0 +1,2 @@
+("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequired7c6.register("d4dcJ",(function(e,o){(()=>{const e={openModalBtn:document.querySelectorAll("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.forEach((e=>e.addEventListener("click",o))),e.closeModalBtn.addEventListener("click",o)})()}));
+//# sourceMappingURL=modal.9d8886af.js.map
